2bedf0f30e5b44f1989f905a2dbad3ff913afc4d
Aziz soorti <hies.aziz2019@gmail.com>
2023-03-02 03:23:05 -0800

    error debuged in password validation

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -2,6 +2,6 @@ import * as yup from 'yup';
 export const validationSchema = yup.object({
     fullName: yup.string().min(2).max(20).required("please enter your name"),
     email: yup.string().email("Invalid email address").required("Email is Required"),
-    password: yup.string().min(" 5 or more then 5 characters Long").required("Required"),
+    password: yup.string().min(5, "Password Must be 5 or more then 5 characters Long").required("Required"),
     confirm_password: yup.string().required("Required").oneOf([yup.ref("password"), null], "Passwords must match")
 })
